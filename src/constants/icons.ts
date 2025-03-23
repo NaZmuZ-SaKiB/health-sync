eb2b26bf7d2ee4ab77f5d58c9437d95ed1a8ac8d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-23 14:36:50 +0600

    add icon constants for improved accessibility and organization

---
+++ src/constants/icons.ts
@@ -0,0 +1,15 @@
+import CallIcon from "@/assets/icons/call-icon.svg";
+import mailIcon from "@/assets/icons/mail-icon.svg";
+import appointmentIcon from "@/assets/icons/appointment-icon.svg";
+import clockIcon from "@/assets/icons/clock-icon.svg";
+import stethoscopeIcon from "@/assets/icons/stethoscope-icon.svg";
+import accountIcon from "@/assets/icons/account-icon.svg";
+
+export const Icons = {
+  CallIcon,
+  mailIcon,
+  appointmentIcon,
+  clockIcon,
+  stethoscopeIcon,
+  accountIcon,
+};
