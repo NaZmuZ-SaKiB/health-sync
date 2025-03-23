6d86a9e5765e900dfcba7d19b4e76e37d8f9df3b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-23 22:56:08 +0600

    feat: integrate Outlet component in Layout for improved routing functionality

--- src/routes/global/Layout.tsx
+++ src/routes/global/Layout.tsx
@@ -1,12 +1,13 @@
 import MainHeader from "@/components/global/layout/MainHeader";
 import TopHeader from "@/components/global/layout/TopHeader";
+import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
     <div className="min-h-svh">
       <TopHeader />
       <MainHeader />
-      <div className="hs-container"></div>
+      <Outlet />
     </div>
   );
 };
