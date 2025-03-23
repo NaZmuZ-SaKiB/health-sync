81c798f148d097f641fef58083d0ce7e7cc8c40d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-23 15:35:18 +0600

    feat: update TopHeader component to require link and add showArrow property

--- src/components/global/layout/TopHeader.tsx
+++ src/components/global/layout/TopHeader.tsx
@@ -6,7 +6,8 @@ type THeaderData = {
   icon: string;
   title: string;
   subTitle: string;
-  link?: string;
+  link: string;
+  showArrow?: boolean;
 };
 
 const headerData: THeaderData[] = [
@@ -14,38 +15,45 @@ const headerData: THeaderData[] = [
     icon: Icons.CallIcon,
     title: "Contact Us",
     subTitle: "+123 456 7890",
+    link: "tel:+1234567890",
   },
   {
     icon: Icons.mailIcon,
     title: "Email Support",
     subTitle: "support@gmail.com",
+    link: "mailto:support@gmail.com",
   },
   {
     icon: Icons.appointmentIcon,
     title: "Online Appointment",
     subTitle: "How to Book",
     link: "/#how-to-book",
+    showArrow: true,
   },
   {
     icon: Icons.stethoscopeIcon,
     title: "Doctors Portal",
     subTitle: "Register/ Login",
     link: "/doctors-portal",
+    showArrow: true,
   },
   {
     icon: Icons.accountIcon,
     title: "Patient Portal",
     subTitle: "Sign In/ Sign Up",
     link: "/sign-in",
+    showArrow: true,
   },
 ];
 
 const TopHeader = () => {
   return (
-    <div className="flex items-center justify-between py-5">
-      {headerData.map((item) => (
-        <TopHeaderItem key={item.title} item={item} />
-      ))}
+    <div className="hs-container">
+      <div className="flex items-center justify-between py-5">
+        {headerData.map((item) => (
+          <TopHeaderItem key={item.title} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -56,16 +64,15 @@ const TopHeaderItem = ({ item }: { item: THeaderData }) => {
       <img src={item.icon} width={40} height={40} alt={item.title} />
       <div>
         <p className="text-primary text-sm font-medium">{item.title}</p>
-        {item?.link ? (
-          <Link to={item.link} className="flex items-center font-semibold">
-            {item.subTitle}{" "}
+
+        <Link to={item.link} className="flex items-center font-semibold">
+          {item.subTitle}{" "}
+          {item.showArrow && (
             <span className="pl-2">
               <MoveRight />
             </span>
-          </Link>
-        ) : (
-          <p className="font-semibold">{item.subTitle}</p>
-        )}
+          )}
+        </Link>
       </div>
     </div>
   );
