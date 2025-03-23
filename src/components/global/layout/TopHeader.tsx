98286281a4afae767b4135f3ebb2f162f74bc174
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-23 14:37:08 +0600

    add TopHeader component with dynamic header items and links

--- src/components/global/layout/TopHeader.tsx
+++ src/components/global/layout/TopHeader.tsx
@@ -1,7 +1,72 @@
+import { Icons } from "@/constants";
+import { MoveRight } from "lucide-react";
+import { Link } from "react-router";
+
+type THeaderData = {
+  icon: string;
+  title: string;
+  subTitle: string;
+  link?: string;
+};
+
+const headerData: THeaderData[] = [
+  {
+    icon: Icons.CallIcon,
+    title: "Contact Us",
+    subTitle: "+123 456 7890",
+  },
+  {
+    icon: Icons.mailIcon,
+    title: "Email Support",
+    subTitle: "support@gmail.com",
+  },
+  {
+    icon: Icons.appointmentIcon,
+    title: "Online Appointment",
+    subTitle: "How to Book",
+    link: "/#how-to-book",
+  },
+  {
+    icon: Icons.stethoscopeIcon,
+    title: "Doctors Portal",
+    subTitle: "Register/ Login",
+    link: "/doctors-portal",
+  },
+  {
+    icon: Icons.accountIcon,
+    title: "Patient Portal",
+    subTitle: "Sign In/ Sign Up",
+    link: "/sign-in",
+  },
+];
+
 const TopHeader = () => {
   return (
-    <div>
-      <h1>TopHeader</h1>
+    <div className="flex items-center justify-between py-5">
+      {headerData.map((item) => (
+        <TopHeaderItem key={item.title} item={item} />
+      ))}
+    </div>
+  );
+};
+
+const TopHeaderItem = ({ item }: { item: THeaderData }) => {
+  return (
+    <div className="flex gap-3">
+      <img src={item.icon} width={40} height={40} alt={item.title} />
+      <div>
+        <p className="text-primary text-sm font-medium">{item.title}</p>
+        {item?.link ? (
+          <Link to={item.link} className="flex items-center font-semibold">
+            {item.subTitle}{" "}
+            <span className="pl-2">
+              <MoveRight />
+            </span>
+          </Link>
+        ) : (
+          <p className="font-semibold">{item.subTitle}</p>
+        )}
+      </div>
     </div>
   );
 };
