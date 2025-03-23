791601bb16415459fd6be827981748bc4acc7ce3
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-23 22:56:14 +0600

    feat: add Home component as a child route in the main router for improved navigation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import Layout from "@/routes/global/Layout";
 import { createBrowserRouter } from "react-router";
+import Home from "./global/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Layout,
+    children: [
+      {
+        path: "/",
+        Component: Home,
+      },
+    ],
   },
 ]);
 
