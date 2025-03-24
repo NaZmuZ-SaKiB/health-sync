fc7c8bc106120f38dc645f63c461a57279720e89
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-24 15:53:02 +0600

    feat: add dummy profile images to images constant

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,6 +3,10 @@ import Logo from "@/assets/images/logo.webp";
 import HeroBG from "@/assets/images/hero-bg.webp";
 import BannerDoctor from "@/assets/images/banner-doctor.webp";
 import QuestionDoctor from "@/assets/images/question-doctor.webp";
+import DummyProfile1 from "@/assets/images/dummy-profile-1.webp";
+import DummyProfile2 from "@/assets/images/dummy-profile-2.webp";
+import DummyProfile3 from "@/assets/images/dummy-profile-3.webp";
+import DummyProfile4 from "@/assets/images/dummy-profile-4.webp";
 
 export const Images = {
   LogoMain,
@@ -10,4 +14,8 @@ export const Images = {
   HeroBG,
   BannerDoctor,
   QuestionDoctor,
+  DummyProfile1,
+  DummyProfile2,
+  DummyProfile3,
+  DummyProfile4,
 };
