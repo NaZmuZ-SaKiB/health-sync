2358553189ebfb75bbbaeae8289f926736136bc8
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-24 23:02:33 +0600

    feat: integrate AboutSection component into Home page

--- src/routes/global/Home/Home.tsx
+++ src/routes/global/Home/Home.tsx
@@ -1,9 +1,11 @@
+import AboutSection from "./_components/AboutSection";
 import HeroSection from "./_components/HeroSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
     </div>
   );
 };
