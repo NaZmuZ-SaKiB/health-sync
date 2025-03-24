1f929e99ccc3646844667dd52c037006e5a5ae84
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-24 15:06:38 +0600

    feat: add QuestionDoctor image to images export for enhanced visual assets

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,10 +2,12 @@ import LogoMain from "@/assets/images/logo-main.webp";
 import Logo from "@/assets/images/logo.webp";
 import HeroBG from "@/assets/images/hero-bg.webp";
 import BannerDoctor from "@/assets/images/banner-doctor.webp";
+import QuestionDoctor from "@/assets/images/question-doctor.webp";
 
 export const Images = {
   LogoMain,
   Logo,
   HeroBG,
   BannerDoctor,
+  QuestionDoctor,
 };
