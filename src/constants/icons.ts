4f00cc6a02f95cacfee4ba1c4b7d6a5d5630d208
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-25 14:51:04 +0600

    feat: add new medical icons for various specialties and update icons constant

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -6,6 +6,15 @@ import StethoscopeIcon from "@/assets/icons/stethoscope-icon.svg";
 import AccountIcon from "@/assets/icons/account-icon.svg";
 import PulseLine from "@/assets/icons/pulse-line.png";
 import InnerRoundCorner from "@/assets/icons/inner-round-corner.svg";
+import InnerRoundedSquare from "@/assets/icons/inner-rounded-square.svg";
+import HeartIcon from "@/assets/icons/heart.png";
+import ENTIcon from "@/assets/icons/ENT.png";
+import DermatologyIcon from "@/assets/icons/dermatology.png";
+import MedicineIcon from "@/assets/icons/medicine.png";
+import NeurologyIcon from "@/assets/icons/neurology.png";
+import OphthalmologyIcon from "@/assets/icons/ophthalmology.png";
+import OrthopedicsIcon from "@/assets/icons/orthopedics.png";
+import UrologyIcon from "@/assets/icons/urology.png";
 
 export const Icons = {
   CallIcon,
@@ -16,4 +25,13 @@ export const Icons = {
   AccountIcon,
   PulseLine,
   InnerRoundCorner,
+  InnerRoundedSquare,
+  HeartIcon,
+  ENTIcon,
+  DermatologyIcon,
+  MedicineIcon,
+  NeurologyIcon,
+  OphthalmologyIcon,
+  OrthopedicsIcon,
+  UrologyIcon,
 };
