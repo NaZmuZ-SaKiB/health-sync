96e4804d81fea53bc4bb3e95dd20d314e2826386
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-28 15:52:51 +0600

    feat: enhance ReviewSection with carousel for patient testimonials

--- src/routes/global/Home/_components/ReviewSection.tsx
+++ src/routes/global/Home/_components/ReviewSection.tsx
@@ -1,10 +1,19 @@
 import { Images } from "@/constants";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Star } from "lucide-react";
+import Autoplay from "embla-carousel-autoplay";
 
 const ReviewSection = () => {
   return (
     <div className="bg-secondary overflow-hidden">
       <div className="hs-container">
-        <div className="grid grid-cols-2 gap-10 pt-24">
+        <div className="grid grid-cols-2 items-center pt-24">
           <div className="relative z-10">
             <img
               className="mx-auto w-full max-w-[500px]"
@@ -33,7 +42,98 @@ const ReviewSection = () => {
               </div>
             </div>
           </div>
-          <div></div>
+          <div className="px-10">
+            <h2 className="mb-7 text-4xl leading-12 font-bold text-slate-50">
+              Real Patients, Real Stories. And our Achievements
+            </h2>
+
+            <div>
+              <Carousel
+                className="w-full"
+                opts={{ loop: true }}
+                plugins={[
+                  Autoplay({
+                    delay: 3000,
+                  }),
+                ]}
+              >
+                <CarouselContent>
+                  <CarouselItem>
+                    <div className="flex w-full items-center overflow-hidden rounded-3xl bg-white">
+                      <div className="w-[210px]">
+                        <img
+                          className="w-full"
+                          src={Images.DummyProfile1}
+                          alt="dummy user 1"
+                        />
+                        <div className="bg-primary/10 py-3 text-center">
+                          <h3 className="text-secondary text-lg font-medium">
+                            Redoy Mollah
+                          </h3>
+                          <p className="text-primary text-sm">Patient</p>
+                        </div>
+                      </div>
+                      {/* Review text  */}
+                      <div className="flex-1 p-5">
+                        {/* Starts  */}
+                        <div className="mb-3 flex">
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                        </div>
+                        <p className="leading-7 text-slate-700">
+                          I had a wonderful experience with Doctor. She was very
+                          attentive, took the time to listen to all my concerns,
+                          and explained everything in a way that was easy to
+                          understand. I highly recommend her to anyone looking
+                          for a caring and knowledgeable doctor.
+                        </p>
+                      </div>
+                    </div>
+                  </CarouselItem>
+                  <CarouselItem>
+                    <div className="flex w-full items-center overflow-hidden rounded-3xl bg-white">
+                      <div className="w-[210px]">
+                        <img
+                          className="w-full"
+                          src={Images.DummyProfile2}
+                          alt="dummy user 1"
+                        />
+                        <div className="bg-primary/10 py-3 text-center">
+                          <h3 className="text-secondary text-lg font-medium">
+                            Jorina Begum
+                          </h3>
+                          <p className="text-primary text-sm">Patient</p>
+                        </div>
+                      </div>
+                      {/* Review text  */}
+                      <div className="flex-1 p-5">
+                        {/* Starts  */}
+                        <div className="mb-3 flex">
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                          <Star className="size-5 fill-yellow-500 text-transparent" />
+                        </div>
+                        <p className="leading-7 text-slate-700">
+                          I had a wonderful experience with Doctor. She was very
+                          attentive, took the time to listen to all my concerns,
+                          and explained everything in a way that was easy to
+                          understand. I highly recommend her to anyone looking
+                          for a caring and knowledgeable doctor.
+                        </p>
+                      </div>
+                    </div>
+                  </CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious className="scale-200 cursor-pointer border-none bg-transparent text-slate-50 opacity-50 hover:bg-transparent hover:text-slate-50 hover:opacity-100" />
+                <CarouselNext className="scale-200 cursor-pointer border-none bg-transparent text-slate-50 opacity-50 hover:bg-transparent hover:text-slate-50 hover:opacity-100" />
+              </Carousel>
+            </div>
+          </div>
         </div>
       </div>
     </div>
