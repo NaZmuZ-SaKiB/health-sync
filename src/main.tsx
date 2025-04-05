97b466c726535bd5fae9ed43b83d122f308e167e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-05 16:03:02 +0600

    feat: integrate Apollo Client for GraphQL data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_BACKEND_URL,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </StrictMode>,
 );
