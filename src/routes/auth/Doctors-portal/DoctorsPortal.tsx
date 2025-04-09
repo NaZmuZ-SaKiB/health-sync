29f574a3c6ebea1d8431dab99336b9d1ac57a7c5
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-09 19:30:42 +0600

    feat: implement DoctorsPortalPage component for doctor registration process

---
+++ src/routes/auth/Doctors-portal/DoctorsPortal.tsx
@@ -0,0 +1,33 @@
+import { Images } from "@/constants";
+import { useState } from "react";
+import DoctorStep1Form from "./_components/DoctorStep1Form";
+import DoctorStep2Form from "./_components/DoctorStep2Form";
+
+const DoctorsPortalPage = () => {
+  const [step, setStep] = useState<1 | 2 | 3>(2);
+
+  const [formData, setFormData] = useState<Record<string, any> | null>(null);
+
+  const renderSteps = {
+    1: () => <DoctorStep1Form setStep={setStep} setFormData={setFormData} />,
+    2: () => <DoctorStep2Form />,
+    3: () => "asdfsd",
+  };
+
+  return (
+    <div
+      className="grid min-h-svh place-items-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${Images.HeroBG})` }}
+    >
+      <div className="bg-primary-hover w-full max-w-[700px] rounded-3xl p-10 shadow-2xl shadow-slate-300">
+        <h1 className="mb-5 text-center text-3xl font-semibold text-slate-50">
+          Doctor Rgistration
+        </h1>
+
+        {renderSteps[step]()}
+      </div>
+    </div>
+  );
+};
+
+export default DoctorsPortalPage;
