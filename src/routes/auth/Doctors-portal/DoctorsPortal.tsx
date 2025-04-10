5ae42e3f1678b05a2faab01813142837cb2b9456
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-10 18:04:44 +0600

    feat: enhance DoctorsPortalPage to support three-step navigation and include DoctorSuccessStep component

--- src/routes/auth/Doctors-portal/DoctorsPortal.tsx
+++ src/routes/auth/Doctors-portal/DoctorsPortal.tsx
@@ -2,15 +2,30 @@ import { Images } from "@/constants";
 import { useState } from "react";
 import DoctorStep1Form from "./_components/DoctorStep1Form";
 import DoctorStep2Form from "./_components/DoctorStep2Form";
+import DoctorSuccessStep from "./_components/DoctorSuccessStep";
 
 const DoctorsPortalPage = () => {
-  const [step, setStep] = useState<1 | 2>(1);
+  const [step, setStep] = useState<1 | 2 | 3>(1);
 
   const [formData, setFormData] = useState<Record<string, any> | null>(null);
 
+  const nextStep = () => {
+    setStep((prevStep) => (prevStep + 1) as any);
+  };
+  const previousStep = () => {
+    setStep((prevStep) => (prevStep - 1) as any);
+  };
+
   const renderSteps = {
-    1: () => <DoctorStep1Form setStep={setStep} setFormData={setFormData} />,
-    2: () => <DoctorStep2Form setStep={setStep} formData={formData} />,
+    1: () => <DoctorStep1Form nextStep={nextStep} setFormData={setFormData} />,
+    2: () => (
+      <DoctorStep2Form
+        prevStep={previousStep}
+        nextStep={nextStep}
+        formData={formData}
+      />
+    ),
+    3: () => <DoctorSuccessStep email={formData?.email} />,
   };
 
   return (
@@ -19,10 +34,6 @@ const DoctorsPortalPage = () => {
       style={{ backgroundImage: `url(${Images.HeroBG})` }}
     >
       <div className="bg-primary-hover w-full max-w-[700px] rounded-3xl p-10 shadow-2xl shadow-slate-300">
-        <h1 className="mb-5 text-center text-3xl font-semibold text-slate-50">
-          Doctor Rgistration
-        </h1>
-
         {renderSteps[step]()}
       </div>
     </div>
