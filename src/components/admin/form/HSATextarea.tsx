e6c1d0867b75e150f555fb6a92dfdf38ec75917d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-12 16:29:20 +0600

    feat: add HSATextarea component for enhanced user input in forms

---
+++ src/components/admin/form/HSATextarea.tsx
@@ -0,0 +1,81 @@
+import { useFormContext } from "react-hook-form";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+type TProps = {
+  name: string;
+  label: string;
+  placeholder?: string;
+  className?: string;
+  vertical?: boolean;
+  description?: string;
+  required?: boolean;
+  disabled?: boolean;
+  readOnly?: boolean;
+};
+
+const HSATextarea = ({
+  name,
+  label,
+  placeholder,
+  className,
+  description,
+  vertical = false,
+  required = true,
+  disabled = false,
+  readOnly = false,
+}: TProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem
+          className={cn("flex w-full flex-col gap-1", {
+            "grid grid-cols-[1fr_2fr] items-center gap-x-2": vertical,
+          })}
+        >
+          <FormLabel className={"gap-0.5 text-xs font-medium text-nowrap"}>
+            {label}
+            {required && <span className="text-red-500">*</span>}
+          </FormLabel>
+          <FormControl>
+            <Textarea
+              placeholder={placeholder || label}
+              className={cn(
+                "focus-visible:border-primary !mt-1 max-h-60 rounded-none bg-slate-50 focus-visible:ring-0 focus-visible:ring-offset-0",
+                {
+                  "!mt-0": vertical,
+                },
+                className,
+              )}
+              disabled={disabled}
+              readOnly={readOnly}
+              {...field}
+            />
+          </FormControl>
+          {description && (
+            <FormDescription className="col-span-2">
+              {description}
+            </FormDescription>
+          )}
+          <FormMessage className="col-span-2 !mt-0 font-normal" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default HSATextarea;
