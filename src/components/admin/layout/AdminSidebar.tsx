ab83abae0ad80655e3f5953dff7b0d562426a2ad
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-12 16:29:34 +0600

    style: update AdminSidebar component styles for improved visual consistency

--- src/components/admin/layout/AdminSidebar.tsx
+++ src/components/admin/layout/AdminSidebar.tsx
@@ -14,17 +14,19 @@ import { Link } from "react-router";
 const AdminSidebar = () => {
   const { state } = useSidebar();
   return (
-    <Sidebar
-      collapsible="icon"
-      className="[&>div]:bg-primary-hover border-none"
-    >
-      <SidebarHeader className="border-primary flex flex-row items-center justify-center gap-3 border-b">
-        <img src={Images.Logo} alt="health sync" height={40} width={40} />
-        {state === "expanded" && (
-          <div className={cn("text-xl font-semibold text-slate-50")}>
-            Health Sync
-          </div>
-        )}
+    <Sidebar collapsible="icon" className="[&>div]:bg-secondary border-none">
+      <SidebarHeader className="mb-5 hover:bg-slate-50/5">
+        <Link
+          to="/"
+          className="flex flex-row items-center justify-center gap-3 py-5"
+        >
+          <img src={Images.Logo} alt="health sync" height={40} width={40} />
+          {state === "expanded" && (
+            <div className={cn("text-xl font-semibold text-slate-50")}>
+              Health Sync
+            </div>
+          )}
+        </Link>
       </SidebarHeader>
 
       <SidebarContent>
