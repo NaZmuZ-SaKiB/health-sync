e0e94f7b84115fe84cafb463cdf425c596d0af39
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-12 16:29:55 +0600

    feat: add APageHeader component for structured page headers with optional back navigation

---
+++ src/components/admin/ui/APageHeader.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { ClassValue } from "clsx";
+import { ChevronLeft } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import ABox from "./ABox";
+import { useNavigate } from "react-router";
+
+type TProps = {
+  title: string;
+  className?: ClassValue;
+  children?: React.ReactNode;
+  backButton?: boolean;
+};
+
+const APageHeader = ({
+  title,
+  className,
+  children,
+  backButton = false,
+}: TProps) => {
+  const navigate = useNavigate();
+  return (
+    <ABox className="flex justify-between">
+      <div className="flex items-center">
+        {backButton && (
+          <span className="cursor-pointer pr-2" onClick={() => navigate(-1)}>
+            <ChevronLeft className="size-7" />
+          </span>
+        )}
+        <h1 className={cn("text-secondary text-xl font-bold", className)}>
+          {title}
+        </h1>
+      </div>
+      {children}
+    </ABox>
+  );
+};
+
+export default APageHeader;
