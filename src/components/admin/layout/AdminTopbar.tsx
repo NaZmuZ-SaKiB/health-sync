fb3d5d56d98f81e4ea305a31ad9f6744bf1af54b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-12 16:29:38 +0600

    style: update AdminTopbar background color for improved visual consistency

--- src/components/admin/layout/AdminTopbar.tsx
+++ src/components/admin/layout/AdminTopbar.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const AdminTopbar = () => {
   return (
-    <div className="flex h-[50px] items-center border-b">
+    <div className="flex h-[50px] items-center bg-white">
       <SidebarTrigger />
     </div>
   );
