75602be0c4920264e99bb4c662b222b4a4ab89a9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-12 16:30:19 +0600

    feat: add validation schema for specialty creation and update

---
+++ src/lib/modules/specialty/specialty.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const create = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Specialty name must have min 3 chars." })
+    .max(255),
+  description: z.string().max(255).optional(),
+  icon: z
+    .string()
+    .nonempty()
+    .url({ message: "Invalid URL for icon." })
+    .optional(),
+});
+
+const update = create.partial().extend({
+  specialtyId: z.string().min(1, { message: "Specialty ID is required." }),
+});
+
+export const SpecialtyValidation = { create, update };
