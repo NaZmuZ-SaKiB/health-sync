e7d561e8fc7b3b8e2a8bd0381a7c4badd8fe8fde
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-12 15:21:13 +0600

    feat: add AdminTopbar component with SidebarTrigger

---
+++ src/components/admin/layout/AdminTopbar.tsx
@@ -0,0 +1,11 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+const AdminTopbar = () => {
+  return (
+    <div className="flex h-[50px] items-center border-b">
+      <SidebarTrigger />
+    </div>
+  );
+};
+
+export default AdminTopbar;
