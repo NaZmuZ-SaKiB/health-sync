dad480286158f4cb105a248e700742e50a63fc20
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-15 21:27:10 +0600

    feat: add LimitFilter component for selecting item limits

---
+++ src/components/admin/shared/LimitFilter.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import { useSearchParams } from "react-router";
+
+type TLimit = "10" | "25" | "50" | "75" | "100";
+
+const limitOptions: TLimit[] = ["10", "25", "50", "75", "100"];
+
+const LimitFilter = ({ defaultValue }: { defaultValue?: TLimit }) => {
+  const [limit, setLimit] = useState<TLimit>(defaultValue ?? "10");
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = new URLSearchParams(searchParams);
+
+  const handleChange = (value: TLimit) => {
+    setLimit(value);
+
+    params.delete("limit");
+    params.delete("page");
+    params.append("page", "1");
+    params.append("limit", value);
+
+    setSearchParams(params);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="focus-visible:border-primary cursor-pointer rounded-none text-slate-700 capitalize hover:bg-slate-50 focus-visible:text-slate-900 focus-visible:ring-0"
+          variant="outline"
+          size="sm"
+        >
+          Show : {limit}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="rounded-none p-0">
+        <DropdownMenuRadioGroup
+          value={limit}
+          onValueChange={handleChange as any}
+        >
+          {limitOptions.map((option) => (
+            <DropdownMenuRadioItem
+              className="cursor-pointer rounded-none text-slate-700 capitalize hover:bg-slate-50 hover:text-slate-900"
+              key={option}
+              value={option}
+            >
+              {option}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default LimitFilter;
