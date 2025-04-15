3653d2f86aee5ef40847d5bf6008d2331fb98e9e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-15 21:27:33 +0600

    feat: integrate selection and sorting features in SpecialtiesPage

--- src/routes/admin/Specialties/Specialties.tsx
+++ src/routes/admin/Specialties/Specialties.tsx
@@ -5,8 +5,17 @@ import SpecialtyCreateForm from "./_components/SpecialtyCreateForm";
 import SpecialtyTable from "./_components/SpecialtyTable";
 import SearchFilter from "@/components/admin/shared/SearchFilter";
 import ABox from "@/components/admin/ui/ABox";
+import SortByFilter from "@/components/admin/shared/SortByFilter";
+import SortOrderFilter from "@/components/admin/shared/SortOrderFilter";
+import LimitFilter from "@/components/admin/shared/LimitFilter";
+import { useState } from "react";
+import SelectedCount from "@/components/admin/shared/SelectedCount";
+
+const specialtySortByOptions = ["name", "createdAt", "updatedAt"];
 
 const SpecialtiesPage = () => {
+  const [selected, setSelected] = useState<string[]>([]);
+
   return (
     <APageContainer>
       <APageHeader title="Specialties" />
@@ -18,9 +27,18 @@ const SpecialtiesPage = () => {
 
         <div className="space-y-4">
           <ABox>
+            <div className="mb-2 flex items-center gap-2">
+              <SelectedCount count={selected.length} />
+              <SortByFilter
+                options={specialtySortByOptions}
+                defaultValueIndex={1}
+              />
+              <SortOrderFilter />
+              <LimitFilter />
+            </div>
             <SearchFilter />
           </ABox>
-          <SpecialtyTable />
+          <SpecialtyTable selected={selected} setSelected={setSelected} />
         </div>
       </AGrid>
     </APageContainer>
