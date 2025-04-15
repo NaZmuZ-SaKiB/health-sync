a8ca06567a157fae4c2cefb3a14000d362e505f4
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-15 21:26:50 +0600

    feat: implement SortByFilter component for enhanced sorting functionality

---
+++ src/components/admin/shared/SortByFilter.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import { useSearchParams } from "react-router";
+
+type TProps = { options: string[]; defaultValueIndex: number };
+
+const SortByFilter = ({ options, defaultValueIndex }: TProps) => {
+  const [sortBy, setSortBy] = useState(options[defaultValueIndex]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = new URLSearchParams(searchParams);
+
+  const handleChange = (value: string) => {
+    setSortBy(value);
+
+    params.delete("sortBy");
+    params.delete("page");
+    params.append("page", "1");
+    params.append("sortBy", value);
+
+    setSearchParams(params);
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="focus-visible:border-primary cursor-pointer rounded-none text-slate-700 capitalize hover:bg-slate-50 focus-visible:text-slate-900 focus-visible:ring-0"
+          variant="outline"
+          size="sm"
+        >
+          Sort By : {sortBy}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="rounded-none p-0">
+        <DropdownMenuRadioGroup value={sortBy} onValueChange={handleChange}>
+          {options.map((option) => (
+            <DropdownMenuRadioItem
+              className="cursor-pointer rounded-none text-slate-700 capitalize hover:bg-slate-50 hover:text-slate-900"
+              key={option}
+              value={option}
+            >
+              {option}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default SortByFilter;
