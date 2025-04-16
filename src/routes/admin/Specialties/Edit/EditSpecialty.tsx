7bd83965abb74891a20b9dddd906cde7e288139c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-16 12:09:26 +0600

    feat: create EditSpecialtyPage component for editing specialty details

---
+++ src/routes/admin/Specialties/Edit/EditSpecialty.tsx
@@ -0,0 +1,44 @@
+import PageLoader from "@/components/admin/shared/PageLoader";
+import AGrid from "@/components/admin/ui/AGrid";
+import APageContainer from "@/components/admin/ui/APageContainer";
+import APageHeader from "@/components/admin/ui/APageHeader";
+import { SpecialtyQueries } from "@/lib/modules/specialty/specialty.queries";
+import { TSpecialty } from "@/lib/modules/specialty/specialty.type";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router";
+import SpecialtyEditForm from "../_components/SpecialtyEditForm";
+
+const EditSpecialtyPage = () => {
+  const params = useParams();
+  const { id } = params;
+
+  const { data: specialtyData, loading: specialtyLoading } = useQuery(
+    SpecialtyQueries.SPECIALTY_BY_ID,
+    {
+      variables: { id },
+    },
+  );
+  const specialty: TSpecialty | undefined = specialtyData?.specialty;
+
+  if (specialtyLoading) {
+    return <PageLoader />;
+  }
+
+  if (!specialtyLoading && !specialty) {
+    return <div>No Specialty Found</div>;
+  }
+
+  return (
+    <APageContainer>
+      <APageHeader title={specialty?.name ?? "Edit"} backButton />
+
+      <AGrid>
+        <div>
+          <SpecialtyEditForm specialty={specialty as TSpecialty} />
+        </div>
+      </AGrid>
+    </APageContainer>
+  );
+};
+
+export default EditSpecialtyPage;
