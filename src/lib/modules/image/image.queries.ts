e9f0cce0489f9f6737d41644ed0c635f890d9462
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-27 23:54:17 +0600

    feat: add GraphQL queries for image management

---
+++ src/lib/modules/image/image.queries.ts
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+
+const IMAGE_LIST = gql`
+  query GetAllImages($page: String, $limit: String, $searchTerm: String) {
+    getAllImages(page: $page, limit: $limit, searchTerm: $searchTerm) {
+      images {
+        id
+        name
+        secureUrl
+        thumbnailUrl
+      }
+      meta {
+        limit
+        page
+        total
+      }
+    }
+  }
+`;
+
+const CREATE_IMAGES = gql`
+  mutation CreateImages($input: [ImagesCreateInput!]!) {
+    createImages(input: $input) {
+      success
+    }
+  }
+`;
+
+export const ImageQueries = { IMAGE_LIST, CREATE_IMAGES };
