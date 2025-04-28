201fe3acada99cb6d0d14e381963d924001bea48
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-28 23:06:31 +0600

    feat: add sidebar items for doctor and admin navigation

---
+++ src/constants/sidebar.ts
@@ -0,0 +1,93 @@
+import {
+  LayoutDashboard,
+  LucideProps,
+  Settings2,
+  Stethoscope,
+  UserRound,
+} from "lucide-react";
+
+export type TAdminSidebarChildItem = {
+  label: string;
+  route: string;
+};
+
+export type TSidebarItem = {
+  label: string;
+  route?: string;
+  icon: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+  children?: TAdminSidebarChildItem[];
+};
+
+export const doctorSidebarItems: TSidebarItem[] = [
+  {
+    label: "Dashboard",
+    route: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "Account",
+    icon: UserRound,
+    children: [
+      {
+        label: "Profile",
+        route: "/dashboard/account/profile",
+      },
+      {
+        label: "Change Password",
+        route: "/dashboard/account/change-password",
+      },
+    ],
+  },
+];
+
+export const adminSidebarItems: TSidebarItem[] = [
+  {
+    label: "Dashboard",
+    route: "/admin",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "Users",
+    icon: UserRound,
+    children: [
+      {
+        label: "Patients",
+        route: "/admin/users/patients",
+      },
+      {
+        label: "Doctors",
+        route: "/admin/users/doctors",
+      },
+      {
+        label: "Admins",
+        route: "/admin/users/admins",
+      },
+    ],
+  },
+  {
+    label: "Doctor Settings",
+    icon: Stethoscope,
+    children: [
+      {
+        label: "Specialties",
+        route: "/admin/specialties",
+      },
+      {
+        label: "Doctor Applications",
+        route: "/admin/doctor-applications",
+      },
+    ],
+  },
+  {
+    label: "Settings",
+    icon: Settings2,
+    children: [
+      {
+        label: "Locations",
+        route: "/admin/locations",
+      },
+    ],
+  },
+];
