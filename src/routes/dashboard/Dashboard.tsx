944964b44786ea80d16efa9f6ea89e83eefb9460
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-28 23:14:13 +0600

    feat: Add Dashboard Page

---
+++ src/routes/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import DPageContainer from "@/components/dashboard/ui/DPageContainer";
+import DPageHeader from "@/components/dashboard/ui/DPageHeader";
+
+const DashboardPage = () => {
+  return (
+    <DPageContainer>
+      <DPageHeader title="Dashboard" />
+    </DPageContainer>
+  );
+};
+
+export default DashboardPage;
