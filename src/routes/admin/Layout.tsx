fec96f51b4a91006de95e05ca6a8527368089eeb
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-28 18:32:06 +0600

    feat: wrap Layout component with ProtectedRoute for role-based access control

--- src/routes/admin/Layout.tsx
+++ src/routes/admin/Layout.tsx
@@ -1,19 +1,23 @@
 import AdminSidebar from "@/components/admin/layout/AdminSidebar";
 import AdminTopbar from "@/components/admin/layout/AdminTopbar";
+import ProtectedRoute from "@/components/global/shared/ProtectedRoute";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { ROLE } from "@/constants";
 import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <div className="min-h-svh bg-slate-100">
-      <SidebarProvider>
-        <AdminSidebar />
-        <div className="flex-1">
-          <AdminTopbar />
-          <Outlet />
-        </div>
-      </SidebarProvider>
-    </div>
+    <ProtectedRoute role={[ROLE.ADMIN, ROLE.SUPER_ADMIN]}>
+      <div className="min-h-svh bg-slate-100">
+        <SidebarProvider>
+          <AdminSidebar />
+          <div className="flex-1">
+            <AdminTopbar />
+            <Outlet />
+          </div>
+        </SidebarProvider>
+      </div>
+    </ProtectedRoute>
   );
 };
 
