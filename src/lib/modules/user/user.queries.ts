dd800efc907fde49279e69d011cd351d667c6857
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-28 23:06:19 +0600

    feat: add PROFILE query to fetch user details including personal and doctor information

--- src/lib/modules/user/user.queries.ts
+++ src/lib/modules/user/user.queries.ts
@@ -11,6 +11,46 @@ const CONTEXT_USER = gql`
   }
 `;
 
+const PROFILE = gql`
+  query Me {
+    me {
+      id
+      email
+      firstName
+      lastName
+      phoneNumber
+      address
+      dateOfBirth
+      gender
+      role
+      profilePicture {
+        publicId
+        secureUrl
+      }
+      patient {
+        emergencyContactName
+        emergencyContactPhone
+        bloodGroup
+        allergies
+      }
+      doctor {
+        specialty {
+          name
+        }
+        location {
+          name
+        }
+        licenseNumber
+        bio
+        qualification
+        experienceYears
+        fee
+      }
+    }
+  }
+`;
+
 export const UserQueries = {
   CONTEXT_USER,
+  PROFILE,
 };
