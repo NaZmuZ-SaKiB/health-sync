bd29a5d691ee54888dbb53c79d12ddb9f83dc0d6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-28 23:14:51 +0600

    feat: Add Dashboard paths

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
 import Layout from "@/routes/global/Layout";
 import AuthLayout from "@/routes/auth/Layout";
 import AdminLayout from "@/routes/admin/Layout";
+import DashboardLayout from "@/routes/dashboard/Layout";
 import { createBrowserRouter } from "react-router";
 import Home from "./global/Home/Home";
 import SignInPage from "./auth/Sign-in/SignIn";
 import SignUpPage from "./auth/Sign-up/SignUp";
 import DoctorsPortalPage from "./auth/Doctors-portal/DoctorsPortal";
-import DashboardPage from "./admin/Dashboard/Dashboard";
+import ADashboardPage from "./admin/Dashboard/ADashboard";
 import SpecialtiesPage from "./admin/Specialties/Specialties";
 import EditSpecialtyPage from "./admin/Specialties/Edit/EditSpecialty";
 import LocationsPage from "./admin/Locations/Locations";
@@ -16,6 +17,8 @@ import DoctorApplications from "./admin/Doctor-Applications/DoctorApplications";
 import SingleDoctorApplication from "./admin/Doctor-Applications/Single-Doctor-Application/SingleDoctorApplication";
 import DoctorsPage from "./admin/Users/Doctors/Doctors";
 import SingleDoctorPage from "./admin/Users/Doctors/Single-Doctor/SingleDoctor";
+import DashboardPage from "./dashboard/Dashboard";
+import ProfilePage from "./dashboard/Account/Profile/Profile";
 
 const router = createBrowserRouter([
   {
@@ -47,12 +50,26 @@ const router = createBrowserRouter([
     ],
   },
   {
+    path: "/dashboard",
+    Component: DashboardLayout,
+    children: [
+      {
+        path: "/dashboard",
+        Component: DashboardPage,
+      },
+      {
+        path: "/dashboard/account/profile",
+        Component: ProfilePage,
+      },
+    ],
+  },
+  {
     path: "/admin",
     Component: AdminLayout,
     children: [
       {
         path: "/admin",
-        Component: DashboardPage,
+        Component: ADashboardPage,
       },
       {
         path: "/admin/users/doctors",
