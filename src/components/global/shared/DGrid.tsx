03f419796c275b317a82b40db014c9aa58cfcac2
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-29 11:23:05 +0600

    feat: Add DGrid component for flexible grid layout

---
+++ src/components/global/shared/DGrid.tsx
@@ -0,0 +1,38 @@
+import { ClassValue } from "clsx";
+
+import { cn } from "@/lib/utils";
+
+type TProps = {
+  children?: React.ReactNode;
+  className?: ClassValue;
+  reverse?: boolean;
+  small?: boolean;
+  equal?: boolean;
+};
+
+const DGrid = ({
+  children,
+  className,
+  reverse = false,
+  small = false,
+  equal = false,
+}: TProps) => {
+  return (
+    <div
+      className={cn(
+        "grid-cols-[40%_1fr] gap-4 @max-5xl:space-y-4 @5xl:grid",
+        {
+          "grid-cols-[1fr_1fr]": equal,
+          "grid-cols-[1fr_40%]": reverse,
+          "grid-cols-[30%_1fr]": small,
+          "grid-cols-[1fr_30%]": reverse && small,
+        },
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default DGrid;
