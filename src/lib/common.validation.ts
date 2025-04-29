b88839bd8c786ef0b380146b8ba69da8ecdb7344
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-29 21:16:28 +0600

    feat: Add bloodGroup validation to CommonValidation

--- src/lib/common.validation.ts
+++ src/lib/common.validation.ts
@@ -1,5 +1,5 @@
-import { doctorVerificationStatuses, genders } from "@/constants";
-import { TDoctorVerificationStatus, TGender } from "@/types";
+import { bloodGroups, doctorVerificationStatuses, genders } from "@/constants";
+import { TBloodGroup, TDoctorVerificationStatus, TGender } from "@/types";
 import { z } from "zod";
 
 const gender = z.enum(genders as [TGender, ...TGender[]], {
@@ -8,14 +8,26 @@ const gender = z.enum(genders as [TGender, ...TGender[]], {
   }),
 });
 
+const bloodGroup = z.enum(bloodGroups as [TBloodGroup, ...TBloodGroup[]], {
+  errorMap: () => ({
+    message: "Invalid Blood Group.",
+  }),
+});
+
 const doctorVerificationStatus = z.enum(
   doctorVerificationStatuses as [
     TDoctorVerificationStatus,
     ...TDoctorVerificationStatus[],
   ],
+  {
+    errorMap: () => ({
+      message: "Invalid verification status.",
+    }),
+  },
 );
 
 export const CommonValidation = {
   gender,
+  bloodGroup,
   doctorVerificationStatus,
 };
