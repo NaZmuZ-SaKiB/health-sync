f31367c0cc0ae9d8f5967e389403452172f80cb2
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-03 22:13:12 +0600

    feat: Remove SelectedCount component as it is no longer needed

--- src/components/admin/shared/SelectedCount.tsx
+++
@@ -1,9 +0,0 @@
-const SelectedCount = ({ count }: { count: number }) => {
-  return (
-    <div className="flex h-8 items-center justify-center gap-1 border px-3 text-sm text-slate-700">
-      <span className="font-medium">{count}</span> Selected
-    </div>
-  );
-};
-
-export default SelectedCount;
