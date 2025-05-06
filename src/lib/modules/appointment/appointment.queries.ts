5810dea20c857d69a8af9a5a263194b96b9494db
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-06 18:12:10 +0600

    feat: add MY_APPOINTMENTS query to fetch all appointments with pagination and sorting

--- src/lib/modules/appointment/appointment.queries.ts
+++ src/lib/modules/appointment/appointment.queries.ts
@@ -1,5 +1,54 @@
 import { gql } from "@apollo/client";
 
+const MY_APPOINTMENTS = gql`
+  query GetAllAppointments(
+    $page: String
+    $limit: String
+    $searchTerm: String
+    $sortBy: String
+    $sortOrder: String
+    $status: APPOINTMENT_STATUS
+    $date: String
+  ) {
+    getAllAppointments(
+      page: $page
+      limit: $limit
+      searchTerm: $searchTerm
+      sortBy: $sortBy
+      sortOrder: $sortOrder
+      status: $status
+      date: $date
+    ) {
+      appointments {
+        id
+        doctor {
+          user {
+            firstName
+            lastName
+          }
+          specialty {
+            name
+          }
+          location {
+            name
+            mapUrl
+          }
+        }
+        timeSlot {
+          slotDate
+          startTime
+          endTime
+        }
+      }
+      meta {
+        page
+        limit
+        total
+      }
+    }
+  }
+`;
+
 const CREATE_APPOINTMENT = gql`
   mutation CreateAppointment($input: AppointmentCreateInput!) {
     createAppointment(input: $input) {
@@ -9,4 +58,4 @@ const CREATE_APPOINTMENT = gql`
   }
 `;
 
-export const AppointmentQueries = { CREATE_APPOINTMENT };
+export const AppointmentQueries = { MY_APPOINTMENTS, CREATE_APPOINTMENT };
