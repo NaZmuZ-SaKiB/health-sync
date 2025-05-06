4b9e94f00e0c81bc37708497a20da8e80e184464
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-06 20:39:33 +0600

    feat: add FieldFilter component for enhanced filtering functionality

---
+++ src/components/global/shared/FieldFilter.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import { useState } from "react";
+import { useSearchParams } from "react-router";
+
+type TProps = {
+  name: string;
+  label: string;
+  options: {
+    label: string;
+    value: string;
+  }[];
+  className?: ClassValue;
+};
+
+const FieldFilter = ({ name, label, options, className }: TProps) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  const [value, setValue] = useState<"all" | string>(params.get(name) || "all");
+
+  const handleChange = (v: string) => {
+    setValue(v);
+
+    params.delete(name);
+    params.delete("page");
+
+    if (v !== "all") {
+      params.append(name, v);
+    }
+
+    setSearchParams(params);
+  };
+
+  const allOptions = [
+    {
+      label: "All",
+      value: "all",
+    },
+    ...options,
+  ];
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className={cn(
+            "focus-visible:border-primary cursor-pointer rounded-none text-slate-700 capitalize hover:bg-slate-50 focus-visible:text-slate-900 focus-visible:ring-0",
+            className,
+          )}
+          variant="outline"
+          size="sm"
+        >
+          {label} : {value}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="rounded-none p-0">
+        <DropdownMenuRadioGroup value={value} onValueChange={handleChange}>
+          {allOptions.map((option) => (
+            <DropdownMenuRadioItem
+              className="cursor-pointer rounded-none text-slate-700 capitalize hover:bg-slate-50 hover:text-slate-900"
+              key={`${name}-filter-${option.value}`}
+              value={option.value}
+            >
+              {option.label}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default FieldFilter;
