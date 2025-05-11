e72f9a130a640787779169af47c23506e48357d3
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-11 21:38:36 +0600

    feat: update TDoctor type to include timeSlots, appointments, and reviews fields

--- src/lib/modules/doctor/doctor.type.ts
+++ src/lib/modules/doctor/doctor.type.ts
@@ -3,6 +3,9 @@ import { TLocation } from "../location/location.type";
 import { TSpecialty } from "../specialty/specialty.type";
 import { TUser } from "../user/user.type";
 import { TDoctorSchedule } from "../doctor-schedule/doctor-schedule.type";
+import { TAppointment } from "../appointment/appointment.type";
+import { TReview } from "../review/review.type";
+import { TTimeSlot } from "../time-slot/time-slot.type";
 
 export type TDoctor = {
   id: string;
@@ -22,7 +25,7 @@ export type TDoctor = {
   specialty: TSpecialty;
   location: TLocation;
   schedules: TDoctorSchedule[];
-  // timeSlots: TTimeSlot[]
-  //   appointments: TAppointment[];
-  //   reviews: TReview[];
+  timeSlots: TTimeSlot[];
+  appointments: TAppointment[];
+  reviews: TReview[];
 };
