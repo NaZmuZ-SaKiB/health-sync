5994c1035b17694d32f1bb065fbceafbc51d0ac0
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-12 12:21:32 +0600

    feat: add SERVICE_LIST and SERVICE_BY_ID queries to ServiceQueries

--- src/lib/modules/service/service.queries.ts
+++ src/lib/modules/service/service.queries.ts
@@ -1,5 +1,47 @@
 import { gql } from "@apollo/client";
 
+const SERVICE_LIST = gql`
+  query GetAllServices(
+    $page: String
+    $limit: String
+    $searchTerm: String
+    $sortBy: String
+    $sortOrder: String
+  ) {
+    getAllServices(
+      page: $page
+      limit: $limit
+      searchTerm: $searchTerm
+      sortBy: $sortBy
+      sortOrder: $sortOrder
+    ) {
+      services {
+        id
+        name
+        icon
+        createdAt
+        updatedAt
+      }
+      meta {
+        page
+        limit
+        total
+      }
+    }
+  }
+`;
+
+const SERVICE_BY_ID = gql`
+  query Service($id: String!) {
+    service(id: $id) {
+      id
+      name
+      description
+      icon
+    }
+  }
+`;
+
 const CREATE_SERVICE = gql`
   mutation CreateService($name: String!, $description: String, $icon: String) {
     createService(name: $name, description: $description, icon: $icon) {
@@ -8,4 +50,4 @@ const CREATE_SERVICE = gql`
   }
 `;
 
-export const ServiceQueries = { CREATE_SERVICE };
+export const ServiceQueries = { SERVICE_LIST, SERVICE_BY_ID, CREATE_SERVICE };
