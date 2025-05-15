2a7c8743d4e528d0558552ca47b93265030b6350
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-15 22:28:20 +0600

    feat: add validation for updating service settings

---
+++ src/lib/modules/service-settings/service-settings.validation.ts
@@ -0,0 +1,20 @@
+import moment from "moment";
+import { z } from "zod";
+
+const timeFormat = "HH:mm";
+
+const update = z.object({
+  startTime: z
+    .string()
+    .refine((time) => moment(time, timeFormat, true).isValid(), {
+      message: "Start time must be in HH:mm format.",
+    }),
+  endTime: z
+    .string()
+    .refine((time) => moment(time, timeFormat, true).isValid(), {
+      message: "End time must be in HH:mm format.",
+    }),
+  duration: z.coerce.number().min(15, "Duration must be at least 15 minutes"),
+});
+
+export const ServiceSettingsValidation = { update };
