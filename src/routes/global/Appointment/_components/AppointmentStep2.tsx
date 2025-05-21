45037c5a0baefb9ef89b34484697793b4cae8adc
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-21 21:35:56 +0600

    refactor: simplify AppointmentStep2 component by removing unused code and enhancing conditional rendering

--- src/routes/global/Appointment/_components/AppointmentStep2.tsx
+++ src/routes/global/Appointment/_components/AppointmentStep2.tsx
@@ -1,10 +1,6 @@
-import HSCalendar from "@/components/global/shared/HSCalendar";
 import { TAppointmentFormData } from "../Appointment";
-import { useEffect, useState } from "react";
-import { useQuery } from "@apollo/client";
-import { DoctorScheduleQueries } from "@/lib/modules/doctor-schedule/doctor-schedule.queries";
-import AppointmentTimeSlots from "./AppointmentTimeSlots";
-import moment from "moment";
+import AppointmentStep2Doctor from "./AppointmentStep2Doctor";
+import AppointmentStep2Service from "./AppointmentStep2Service";
 
 type TProps = {
   formData: Partial<TAppointmentFormData>;
@@ -13,71 +9,27 @@ type TProps = {
 };
 
 const AppointmentStep2 = ({ formData, setFormData, setStep }: TProps) => {
-  const [date, setDate] = useState<Date>(
-    formData?.appointment?.timeSlot?.slotDate
-      ? new Date(
-          moment(
-            formData?.appointment?.timeSlot?.slotDate,
-            "DD-MM-YYYY",
-          ).format("YYYY-MM-DD"),
-        )
-      : new Date(),
-  );
+  if (formData?.appointment?.doctorId) {
+    return (
+      <AppointmentStep2Doctor
+        formData={formData}
+        setFormData={setFormData}
+        setStep={setStep}
+      />
+    );
+  }
 
-  useEffect(() => {
-    const formattedDate = moment(date).format("DD-MM-YYYY");
+  if (formData?.appointment?.serviceId) {
+    return (
+      <AppointmentStep2Service
+        formData={formData}
+        setFormData={setFormData}
+        setStep={setStep}
+      />
+    );
+  }
 
-    if (formData?.appointment?.timeSlot?.slotDate !== formattedDate) {
-      setFormData((prev: any) => ({
-        ...prev,
-        appointment: {
-          ...prev?.appointment,
-          timeSlot: {
-            slotDate: formattedDate,
-          },
-        },
-      }));
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [date]);
-
-  const { data: scheduleData, loading } = useQuery(
-    DoctorScheduleQueries.GET_DOCTOR_SCHEDULE_BY_DATE,
-    {
-      variables: {
-        doctorId: formData?.appointment?.doctorId,
-        date: date?.toDateString(),
-      },
-      skip: !date,
-    },
-  );
-
-  return (
-    <div className="bg-primary-hover mx-auto mb-10 w-full max-w-[800px] space-y-10 rounded-3xl p-10 text-slate-50 shadow-2xl shadow-slate-300">
-      <div>
-        <label>
-          <p className="mb-2 font-semibold">Appointment Date</p>
-          <HSCalendar
-            defaultValue={date}
-            setDate={setDate}
-            fromDate={new Date()}
-            toDate={new Date(Date.now() + 2 * 24 * 60 * 60 * 1000)}
-            className="max-w-[300px]"
-          />
-        </label>
-      </div>
-
-      {!loading && scheduleData?.getDoctorScheduleByDate && (
-        <AppointmentTimeSlots
-          schedule={scheduleData?.getDoctorScheduleByDate}
-          formData={formData}
-          setFormData={setFormData}
-          setStep={setStep}
-        />
-      )}
-    </div>
-  );
+  setStep(1);
 };
 
 export default AppointmentStep2;
