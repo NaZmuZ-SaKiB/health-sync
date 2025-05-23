7f32e709329b47a13e48f22f001b28de8c05c8ac
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-23 21:06:22 +0600

    feat: add location field to MY_APPOINTMENTS query for improved appointment details

--- src/lib/modules/appointment/appointment.queries.ts
+++ src/lib/modules/appointment/appointment.queries.ts
@@ -38,6 +38,9 @@ const MY_APPOINTMENTS = gql`
         service {
           name
         }
+        location {
+          name
+        }
         timeSlot {
           slotDate
           startTime
