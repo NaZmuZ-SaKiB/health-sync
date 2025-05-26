28616719832af3f7958471444af4eb174682d207
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-26 18:34:19 +0600

    feat: include locationId in GetTimeSlotsByDate query for time slot retrieval

--- src/lib/modules/time-slot/time-slot.queries.ts
+++ src/lib/modules/time-slot/time-slot.queries.ts
@@ -4,11 +4,13 @@ const GET_TIME_SLOT_BY_DATE = gql`
   query GetTimeSlotsByDate(
     $doctorId: String
     $serviceId: String
+    $locationId: String
     $date: String!
   ) {
     getTimeSlotsByDate(
       doctorId: $doctorId
       serviceId: $serviceId
+      locationId: $locationId
       date: $date
     ) {
       startTime
