dddbeb31de79e9ad23b7d56ae84e0090f3ad67ed
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-26 19:39:49 +0600

    feat: integrate reportType validation into prescription schema

--- src/lib/modules/medical-report/medical-report.validation.ts
+++ src/lib/modules/medical-report/medical-report.validation.ts
@@ -1,9 +1,11 @@
+import { CommonValidation } from "@/lib/common.validation";
 import { z } from "zod";
 
 const prescriptionSchema = z.object({
   title: z.string().nonempty("Title is Required"),
   notes: z.string().nonempty("Notes is Required"),
   fileUrl: z.string().optional(),
+  reportType: CommonValidation.reportType,
 });
 
 export const MedicalReportValidation = {
