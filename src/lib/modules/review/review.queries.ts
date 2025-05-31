5f64cc8d2b24ad6b2d9528076e1619f0cfd9d14b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-31 21:12:34 +0600

    feat: add REVIEW_LIST query to fetch all reviews with pagination and filtering

--- src/lib/modules/review/review.queries.ts
+++ src/lib/modules/review/review.queries.ts
@@ -1,5 +1,55 @@
 import { gql } from "@apollo/client";
 
+const REVIEW_LIST = gql`
+  query GetAllReviews(
+    $page: String
+    $limit: String
+    $searchTerm: String
+    $sortBy: String
+    $sortOrder: String
+    $type: String
+    $serviceId: String
+  ) {
+    getAllReviews(
+      page: $page
+      limit: $limit
+      searchTerm: $searchTerm
+      sortBy: $sortBy
+      sortOrder: $sortOrder
+      type: $type
+      serviceId: $serviceId
+    ) {
+      meta {
+        page
+        limit
+        total
+      }
+      reviews {
+        id
+        rating
+        comment
+        doctor {
+          user {
+            firstName
+            lastName
+          }
+          id
+        }
+        patient {
+          id
+          user {
+            firstName
+            lastName
+          }
+        }
+        service {
+          name
+        }
+      }
+    }
+  }
+`;
+
 const CREATE_REVIEW = gql`
   mutation CreateReview(
     $appointmentId: String!
@@ -16,4 +66,4 @@ const CREATE_REVIEW = gql`
   }
 `;
 
-export const ReviewQueries = { CREATE_REVIEW };
+export const ReviewQueries = { REVIEW_LIST, CREATE_REVIEW };
