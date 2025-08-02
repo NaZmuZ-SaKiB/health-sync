68d47ea1a961727fb8926cf1986ea99dc192e7c5
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-02 22:13:13 +0600

    Add payment list and user payment queries

--- src/lib/modules/payment/payment.queries.ts
+++ src/lib/modules/payment/payment.queries.ts
@@ -1,5 +1,87 @@
 import { gql } from "@apollo/client";
 
+const MY_PAYMENTS = gql`
+  query GetAllPayments(
+    $page: String
+    $limit: String
+    $searchTerm: String
+    $sortBy: String
+    $sortOrder: String
+    $status: PAYMENT_STATUS
+  ) {
+    getAllPayments(
+      page: $page
+      limit: $limit
+      searchTerm: $searchTerm
+      sortBy: $sortBy
+      sortOrder: $sortOrder
+      status: $status
+    ) {
+      meta {
+        limit
+        page
+        total
+      }
+      payments {
+        amount
+        createdAt
+        id
+        appointment {
+          doctor {
+            user {
+              firstName
+              lastName
+            }
+          }
+        }
+        status
+        details
+      }
+    }
+  }
+`;
+
+const PAYMENT_LIST = gql`
+  query GetAllPayments(
+    $page: String
+    $limit: String
+    $searchTerm: String
+    $sortBy: String
+    $sortOrder: String
+    $status: PAYMENT_STATUS
+  ) {
+    getAllPayments(
+      page: $page
+      limit: $limit
+      searchTerm: $searchTerm
+      sortBy: $sortBy
+      sortOrder: $sortOrder
+      status: $status
+    ) {
+      meta {
+        limit
+        page
+        total
+      }
+      payments {
+        amount
+        createdAt
+        id
+        appointment {
+          patient {
+            user {
+              email
+            }
+            id
+          }
+        }
+        status
+        details
+      }
+    }
+  }
+`;
+
 const PAYMENT_INIT = gql`
   mutation PaymentInit($appointmentId: String!) {
     paymentInit(appointmentId: $appointmentId)
@@ -7,5 +89,7 @@ const PAYMENT_INIT = gql`
 `;
 
 export const PaymentQueries = {
+  MY_PAYMENTS,
+  PAYMENT_LIST,
   PAYMENT_INIT,
 };
