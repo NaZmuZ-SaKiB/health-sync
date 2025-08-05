68611599a9b427d7ee4c127399ab137de0f0a056
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-05 22:42:55 +0600

    Set toast notifications to appear at top-center

--- src/main.tsx
+++ src/main.tsx
@@ -26,7 +26,7 @@ createRoot(document.getElementById("root")!).render(
           <RouterProvider router={router} />
         </UserProvider>
       </CookiesProvider>
-      <Toaster closeButton richColors />
+      <Toaster closeButton richColors position="top-center" />
     </ApolloProvider>
   </StrictMode>,
 );
