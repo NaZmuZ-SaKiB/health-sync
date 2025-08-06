e7e24fd41686a19afe51f29d7922f05b2840d10a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-06 22:42:13 +0600

    Add setting validation schema using Zod

---
+++ src/lib/modules/setting/setting.validation.ts
@@ -0,0 +1,80 @@
+import { z } from "zod";
+import {
+  CONFIG_ABOUT_LIST,
+  CONFIG_ABOUT_TEXT,
+  CONFIG_FEATURED_DOCTOR,
+  CONFIG_FOOTER_TEXT,
+  CONFIG_HERO_IMAGE,
+  CONFIG_HERO_REVIEW,
+  CONFIG_HERO_SPECIALTY_ICON,
+  CONFIG_HERO_SUBTITLE_TEXT,
+  CONFIG_HERO_TITLE_TEXT,
+  CONFIG_OPENING_HOURS,
+  CONFIG_PRESIDENT_EXPERIENCE,
+  CONFIG_PRESIDENT_IMAGE,
+  CONFIG_PRESIDENT_NAME,
+  CONFIG_PRESIDENT_POSITION,
+  CONFIG_PRESIDENT_SKILLS,
+  CONFIG_PRESIDENT_TEXT,
+  CONFIG_TESTIMONIAL_IMAGE,
+} from "./setting.constant";
+
+const update = z.object({
+  key: z
+    .string({
+      required_error: "Key is required",
+    })
+    .nonempty({
+      message: "Key is required",
+    }),
+
+  value: z
+    .string({
+      required_error: "Value is required",
+    })
+    .nonempty({
+      message: "Value is required",
+    }),
+});
+
+const updateMany = z.array(update);
+
+const homepage = z.object({
+  [CONFIG_HERO_TITLE_TEXT]: z.string().nonempty().optional(),
+  [CONFIG_HERO_SUBTITLE_TEXT]: z.string().nonempty().optional(),
+  [CONFIG_FEATURED_DOCTOR]: z.string().nonempty().optional(),
+  [CONFIG_HERO_REVIEW]: z.string().nonempty().optional(),
+  [CONFIG_HERO_IMAGE]: z.string().nonempty().optional(),
+  [CONFIG_HERO_SPECIALTY_ICON]: z.string().nonempty().optional(),
+
+  [CONFIG_ABOUT_TEXT]: z.string().nonempty().optional(),
+  [CONFIG_ABOUT_LIST]: z.string().nonempty().optional(),
+  [CONFIG_OPENING_HOURS]: z
+    .object({
+      monday: z.string().nonempty().max(255).optional(),
+      tuesday: z.string().nonempty().max(255).optional(),
+      wednesday: z.string().nonempty().max(255).optional(),
+      thursday: z.string().nonempty().max(255).optional(),
+      friday: z.string().nonempty().max(255).optional(),
+      saturday: z.string().nonempty().max(255).optional(),
+      sunday: z.string().nonempty().max(255).optional(),
+    })
+    .optional(),
+
+  [CONFIG_TESTIMONIAL_IMAGE]: z.string().nonempty().optional(),
+
+  [CONFIG_PRESIDENT_NAME]: z.string().nonempty().optional(),
+  [CONFIG_PRESIDENT_POSITION]: z.string().nonempty().optional(),
+  [CONFIG_PRESIDENT_EXPERIENCE]: z.coerce.number().min(0).max(50).optional(),
+  [CONFIG_PRESIDENT_TEXT]: z.string().nonempty().optional(),
+  [CONFIG_PRESIDENT_SKILLS]: z.string().nonempty().optional(),
+  [CONFIG_PRESIDENT_IMAGE]: z.string().nonempty().optional(),
+
+  [CONFIG_FOOTER_TEXT]: z.string().nonempty().optional(),
+});
+
+export const SettingValidation = {
+  update,
+  updateMany,
+  homepage,
+};
