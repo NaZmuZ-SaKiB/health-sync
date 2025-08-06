9fffec2657a8257b720ebd1e13a3a9776e832a5d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-06 22:42:01 +0600

    Add configuration constants for website sections Add GraphQL queries for settings management

---
+++ src/lib/modules/setting/setting.queries.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+
+const GET_SETTING_BY_KEY = gql`
+  query Setting($key: String!) {
+    setting(key: $key) {
+      key
+      value
+    }
+  }
+`;
+
+const GET_SETTINGS_BY_KEYS = gql`
+  query Settings($keys: [String!]!) {
+    settings(keys: $keys) {
+      key
+      value
+    }
+  }
+`;
+
+const UPDATE_SETTING = gql`
+  mutation UpdateSetting($key: String!, $value: String!) {
+    updateSetting(key: $key, value: $value) {
+      success
+    }
+  }
+`;
+
+const UPDATE_MANY_SETTINGS = gql``;
+
+export const SettingQueries = {
+  GET_SETTING_BY_KEY,
+  GET_SETTINGS_BY_KEYS,
+  UPDATE_SETTING,
+  UPDATE_MANY_SETTINGS,
+};
