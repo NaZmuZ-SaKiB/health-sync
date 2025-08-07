62d1d713bbc50af690f25aaa90733f068d8b45f7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-07 14:06:04 +0600

    Add className prop to RatingStars component

--- src/components/global/shared/RatingStars.tsx
+++ src/components/global/shared/RatingStars.tsx
@@ -1,10 +1,13 @@
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
 import { Star } from "lucide-react";
 
 type TProps = {
   rating: number;
+  className?: ClassValue;
 };
 
-const RatingStars = ({ rating }: TProps) => {
+const RatingStars = ({ rating, className }: TProps) => {
   return (
     <div className="flex gap-1">
       {Array(5)
@@ -12,9 +15,19 @@ const RatingStars = ({ rating }: TProps) => {
         .map((_, i: number) => (
           <span key={`rating-${i}-${Math.random().toFixed(2)}`} className="">
             {rating >= i + 1 ? (
-              <Star className="cursor-pointer fill-yellow-500 stroke-yellow-500" />
+              <Star
+                className={cn(
+                  "cursor-pointer fill-yellow-500 stroke-yellow-500",
+                  className,
+                )}
+              />
             ) : (
-              <Star className="cursor-pointer fill-slate-300 stroke-slate-300" />
+              <Star
+                className={cn(
+                  "cursor-pointer fill-slate-300 stroke-slate-300",
+                  className,
+                )}
+              />
             )}
           </span>
         ))}
