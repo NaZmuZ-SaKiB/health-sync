4399028cd49cbf6fbb2e9dd2a04a2ba7c8aff299
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-09 14:13:09 +0600

    Refactor homepage settings into separate component

--- src/routes/admin/Setting/Homepage/HomepageSetting.tsx
+++ src/routes/admin/Setting/Homepage/HomepageSetting.tsx
@@ -1,81 +1,19 @@
 import APageContainer from "@/components/admin/ui/APageContainer";
 import APageHeader from "@/components/admin/ui/APageHeader";
-import DGrid from "@/components/global/shared/DGrid";
 import HSButton from "@/components/global/shared/HSButton";
-import { Form } from "@/components/ui/form";
-import { SettingValidation } from "@/lib/modules/setting/setting.validation";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
-import HeroSectionSettings from "./_components/HeroSectionSettings";
-import AboutSectionSetting from "./_components/AboutSectionSetting";
-import PresidentSectionSetting from "./_components/PresidentSectionSetting";
-import FeaturedServicesSetting from "./_components/FeaturedServicesSetting";
-import HSImageUpload from "@/components/global/form/HSImageUpload";
-import { CONFIG_TESTIMONIAL_IMAGE } from "@/lib/modules/setting/setting.constant";
-import ABox from "@/components/admin/ui/ABox";
-import AFormH2 from "@/components/admin/ui/AFormH2";
-import FeaturedSpecialtiesSetting from "./_components/FeaturedSpecialtiesSetting";
-import FAQSetting from "./_components/FAQSetting";
-import { TSetting } from "@/lib/modules/setting/setting.type";
-import { useCookies } from "react-cookie";
-import { AUTH_KEY } from "@/constants";
-import { useMutation } from "@apollo/client";
-import { SettingQueries } from "@/lib/modules/setting/setting.queries";
-import { toast } from "sonner";
+import HomepageSettingForm from "./_components/HomepageSettingForm";
+import { gql, useQuery } from "@apollo/client";
 
-type TForm = z.infer<typeof SettingValidation.homepage>;
+const HOMEPAGE_SETTING = gql`
+  query Query {
+    homepageSetting
+  }
+`;
 
 const HomepageSettingPage = () => {
-  const [cookies] = useCookies([AUTH_KEY]);
+  const { data, loading } = useQuery(HOMEPAGE_SETTING);
 
-  const [updateSettingFn] = useMutation(SettingQueries.UPDATE_MANY_SETTINGS, {
-    context: {
-      headers: {
-        Authorization: cookies[AUTH_KEY] || "",
-      },
-    },
-  });
-
-  const form = useForm<TForm>({
-    resolver: zodResolver(SettingValidation.homepage),
-    mode: "onBlur",
-  });
-
-  const saveHomepageSetting: SubmitHandler<TForm> = async (data) => {
-    const updateData: Pick<TSetting, "key" | "value">[] = [];
-
-    (Object.keys(data) as (keyof TForm)[]).forEach((key) => {
-      if (data[key]) {
-        updateData.push({
-          key: key,
-          value:
-            typeof data[key] === "string"
-              ? data[key]
-              : JSON.stringify(data[key]),
-        });
-      }
-    });
-
-    console.log("update data", updateData);
-
-    try {
-      toast.promise(
-        async () => {
-          return await updateSettingFn({ variables: { settings: updateData } });
-        },
-        {
-          loading: "Saving Homepage Setting...",
-          success: () => {
-            return "Settings Saved.";
-          },
-          error: (error: any) => error?.message,
-        },
-      );
-    } catch {
-      toast.error("A Client error occurred");
-    }
-  };
+  if (loading) return <div>Loading...</div>;
 
   return (
     <APageContainer>
@@ -89,43 +27,7 @@ const HomepageSettingPage = () => {
         </HSButton>
       </APageHeader>
 
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(saveHomepageSetting)}
-          id="homepage-setting-form"
-        >
-          <DGrid equal>
-            <div className="space-y-3">
-              <HeroSectionSettings />
-
-              <AboutSectionSetting />
-
-              <PresidentSectionSetting />
-            </div>
-
-            <div className="space-y-3">
-              <ABox className="mb-3">
-                <AFormH2>Testimonial Section</AFormH2>
-
-                <HSImageUpload
-                  name={CONFIG_TESTIMONIAL_IMAGE}
-                  label={CONFIG_TESTIMONIAL_IMAGE.toLowerCase()
-                    .split("_")
-                    .join(" ")}
-                  defaultValue={[]}
-                  reset={false}
-                />
-              </ABox>
-
-              <FeaturedServicesSetting />
-
-              <FeaturedSpecialtiesSetting />
-
-              <FAQSetting />
-            </div>
-          </DGrid>
-        </form>
-      </Form>
+      <HomepageSettingForm homepageSettings={data?.homepageSetting} />
     </APageContainer>
   );
 };
